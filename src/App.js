61db4661cb93b24e253faa50b00f3a64bc8e91ac
das0166 <das0166@naver.com>
2022-09-21 16:32:41 +0900

    [2022-09-21] 홈화면 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
-
+import Header from './header.js';
+import './App.css';
+import './main.css';
 
 function App() {
+  return(
+    <div>
+      <Header/>
+      <div className='content'>
+        <div className='left'>
+          <div className='sp'>
+            <div><div className='line'></div><div className='circle'></div></div>
+            <div><div className='line'></div><div className='circle'></div></div>
+            <div><div className='line'></div><div className='circle'></div></div>
+            <div><div className='line'></div><div className='circle'></div></div>
+            <div><div className='line'></div><div className='circle'></div></div>
+            <div><div className='line'></div><div className='circle'></div></div>
+          </div>
+          
+        </div>
+        <div className='right'>
+          <div className='sticker'></div>
+        
+          
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
