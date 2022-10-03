8c876b4454c56cde6615d7adca46ff4caa855677
das0166 <das0166@naver.com>
2022-10-03 18:13:14 +0900

    [2022-10-03]option 버튼

--- src/Main/Todo.js
+++ src/Main/Todo.js
@@ -1,6 +1,16 @@
 import React from "react";
+import option from '../image/option.png';
 
 function Todo(){
+/*  <script>
+ const option_button = function(){
+    $('#option').click(function(){
+      if($('#option').css("display") == "none"){
+        $('#option_layout').show();
+      }
+    })
+  }
+</script> */
     return(
         <div className='content'>
         <div className='left'>
@@ -12,6 +22,13 @@ function Todo(){
             <div><div className='line'></div><div className='circle'></div></div>
             <div><div className='line'></div><div className='circle'></div></div>
           </div>
+          <div className="option" id="option">
+            <img className='option_ico' src={option} alt='option'/>
+          </div>
+          <div id="option_layout">
+              <button id="revise">내용 수정하기</button>
+              <button id="color_revise">색상 수정하기</button>
+          </div>
           
         </div>
         <div className='right'>
@@ -21,6 +38,9 @@ function Todo(){
         </div>
       </div>
     )
+
+    
 }
+
 
 export default Todo;
