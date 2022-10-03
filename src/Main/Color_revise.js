e414f6e593c93761c7fa7673838f3cff84cdb51f
das0166 <das0166@naver.com>
2022-10-04 01:49:56 +0900

    [2022-10-04]색상변경 레이아웃 수정

---
+++ src/Main/Color_revise.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Color_revise.css';
+
+function Color_revise(){
+   
+       return(
+            <div className="color_layer">
+                <h1>컬러 수정</h1>
+            </div>
+
+       )
+   
+       
+   }
+   
+   
+   export default Color_revise;
