d56417ee248724f4525d827bcbe664a76bfad283
giryong <giryong99@naver.com>
2022-10-12 00:48:11 +0900

    [2022.10.12] year 제외 디자인 완성

--- src/Main/Calendar/Calendar.js
+++ src/Main/Calendar/Calendar.js
@@ -13,7 +13,27 @@ function Calendar(){
       <FullCalendar
         defaultView="dayGridMonth"
         plugins={[ dayGridPlugin ]}
+        headerToolbar={{
+          left:'prev,next today',
+          center: 'title',
+          right: 'year,dayGridMonth,dayGridWeek,dayGridDay'
+        }}
+        events={[
+          { title: 'event1', date: '2022-10-11'},
+          { title: 'event2', date: '2022-10-12'}
+        ]}
+        titleFormat={function(date){
+          return date.date.year + "년" + (date.date.month+1) + "월";
+        }}
+        columnHeaderText={function(date){
+          var weekList=['일','월','화','수','목','금','토'];
+          return weekList[date.getDay()];
+        }}
+        
       />
+      <div className="plan">
+        <button className="add">일정 추가</button>
+      </div>
     </div>
   )
 }
