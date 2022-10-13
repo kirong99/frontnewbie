9ee40893afa336b1a5daabe6dd2aab6ccb5e89ef
giryong <giryong99@naver.com>
2022-10-13 10:01:07 +0900

    [2022.10.12] 임시 커밋

--- src/Main/Option.js
+++ src/Main/Option.js
@@ -2,25 +2,25 @@ import '../main.css';
 import ColorRevise from './Color_revise';
 import React, { useState } from "react";
 
-function Option(){
+function Option() {
     const [color, setColor] = useState(false);
-   
-       return(
+
+    return (
         <div>
-             <div className="option_layout">
-                 <button className="revise">내용 수정하기</button>
-                 <button className="color_revise"
-                 onClick={() => {
-                    setColor(!color);
-                  }}
-                 >색상 수정하기</button>
-             </div>
-            {color && <ColorRevise />}
+            <div className="option_layout">
+                <button className="revise">내용 수정하기</button>
+                <button className="color_revise"
+                    onClick={() => {
+                        setColor(!color);
+                    }}
+                >색상 수정하기</button>
             </div>
-       )
-   
-       
-   }
-   
-   
-   export default Option;
+            {color && <ColorRevise />}
+        </div>
+    )
+
+
+}
+
+
+export default Option;
