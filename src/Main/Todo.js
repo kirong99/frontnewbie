2bc864bebdc540db819697946cbd6e2d13fef388
giryong <giryong99@naver.com>
2022-10-22 23:53:27 +0900

    [2022.10.22] 메모지 일정 추가

--- src/Main/Todo.js
+++ src/Main/Todo.js
@@ -2,15 +2,17 @@ import React, { useState, useContext} from "react";
 import Note from "./Note";
 import SelectColors from "./SelectColor";
 import { ColorProvider } from './Color';
+import { da } from "date-fns/locale";
+import Moment from 'moment'
 
-function Todo(){
+function Todo( { event }){
     const reset = document.getElementsByClassName("left");
     const [color,setColor] = useState(false);
     const tab = useState('white');
     const reset_note = () => {
       reset = {tab}
     }
-  
+    const formatDate = Moment().format('YYYY-MM-DD');
     return(
       <div className='content'>
         <ColorProvider>
@@ -23,7 +25,15 @@ function Todo(){
           </div>
         </ColorProvider>
         <div className='right'>
-          <div className='sticker'></div>
+          <div className='sticker'>
+          </div>
+          <div>
+              {event.map((it, idx)=>(
+                <div key={idx}>
+                  <div style={{display: formatDate === it.start ? "flex" : "none"}}>일정 : {it.title}</div>
+                </div>
+              ))}
+            </div>
         </div>
       </div>
   )
