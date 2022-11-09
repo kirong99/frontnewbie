7a3e9929e06ee38e11f24c20c294e7f2e181c171
giryong <giryong99@naver.com>
2022-11-09 14:07:32 +0900

    임시 커밋

--- src/Main/Note.js
+++ src/Main/Note.js
@@ -6,35 +6,6 @@ import Option from './Option';
 const Note = () => {
     const {state} = useContext(ColorContext);
     const [option_dropdown, setOption] = useState(false);
-    const [todoState,setTodoState] = useState({title:""})
-
-    const [todoData,setTodoDate] = useState();
-    const dataId = useRef(0);
-
-    const onCreate = (title) => {
-      const newItem = {
-        title,
-        id: dataId.current
-      }
-      dataId.current += 1;
-      setTodoDate([newItem , ...todoData])
-      console.log(todoData)
-    }
-
-    const handleChangeState = (e) => {
-      setTodoDate({
-          ...todoState,
-          [e.target.name] : e.target.value,
-      })
-  }
-
-  const handleSubmit = () => {
-      onCreate(todoState.title);
-      alert('저장 성공');
-      setTodoDate({
-          title: "",
-      });
-  }
 
     return(
           <div className='left' style={{background: state.color}}>
@@ -47,8 +18,8 @@ const Note = () => {
               <div><div className='line'></div><div className='circle'></div></div>
             </div>
             <div className='addTodo'>
-              <input type="text" name="title" value={todoState.title} onChange={handleChangeState}></input>
-              <input type="submit" onClick={handleSubmit}></input>
+              <input type="text" name="title"></input>
+              <input type="submit"></input>
             </div>
           </div>
     )
