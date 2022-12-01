6c345a769becfffadae77879db2f807ba874b190
giryong <giryong99@naver.com>
2022-12-01 10:59:51 +0900

    에러 커밋

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './Main/header.js';
 import Todo from './Main/Todo.js';
 import Calendar from './Main/Calendar/Calendar';
-import { HashRouter as Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import './main.css';
 
@@ -15,5 +15,9 @@ function App() {
       </Route>
     </Routes>
 )}
+
+// 에러 주석
+// You cannot render a <Router> inside another <Router>. You should never have more than one in your app
+// DevTools failed to load source map: Could not load content for chrome-extension://cfhdojbkjhnklbpkdaibdccddilifddb/browser-polyfill.js.map: System error: net::ERR_FILE_NOT_FOUND
 
 export default App;
